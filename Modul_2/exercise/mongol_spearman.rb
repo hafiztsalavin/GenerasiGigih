b285df4d8a153facfcebe6be5633434d7e87bfe8
Hafiz Tsalavin <hafiztsalavin@gmail.com>
2021-07-09 10:25:50 +0700

    Push

--- Modul_2/exercise/mongol_spearman.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'villain'
-
-
-class MongolSpearman < Villain
-    def attack(other_person)
-        puts "#{@name} thrusts #{other_person.name} with #{@attack_damage} damage"
-        other_person.take_damage(@attack_damage)
-    end
-end
--- Modul_2/exercise/samurai.rb
+++
@@ -1,7 +0,0 @@
-require_relative "person"
-
-class Samurai < Person
-    def initialize(name, hit_points, attack_damage)
-        super(name, hit_points, attack_damage)
-    end
-end
